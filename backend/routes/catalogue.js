512aa5258ddf73ac65e31baa879b4e16d9933fdc
VincentSastra <vincent.sastra@gmail.com>
2022-01-19 09:17:06 -0800

    Added endpoints for deleted and restore

--- backend/routes/catalogue.js
+++ backend/routes/catalogue.js
@@ -1,19 +1,49 @@
-var express = require('express');
-const { pendingConnection } = require('../util/db');
+var express = require("express");
+const { pendingConnection } = require("../util/db");
 var router = express.Router();
 
-router.get('/list', async function (req, res) {
-	const connection = await pendingConnection
-	connection
-		.collection("items")
-		.find({}).limit(50)
-		.toArray(function (err, result) {
-			if (err) {
-				res.status(400).send("Error fetching listings!")
-			} else {
-				res.json(result)
-			}
-		})
+router.get("/list", async function (req, res) {
+  const connection = await pendingConnection;
+  connection
+    .collection("items")
+    .find({ deleted: false })
+    .limit(50)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching listings!");
+      } else {
+        res.json(result);
+      }
+    });
+});
+
+router.get("deleted", async function (req, res) {
+  const connection = await pendingConnection;
+  connection
+    .collection("items")
+    .find({ deleted: true })
+    .limit(50)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching listings!");
+      } else {
+        res.json(result);
+      }
+    });
+});
+
+router.post("restore/:id", async function (req, res) {
+  const connection = await pendingConnection;
+  connection.collection("items").updateOne(
+    { name: req.params.id },
+    {
+      $set: {
+        deleted: false,
+      },
+      $unset: "deleteComment",
+    },
+    (err, result) => standardReply(res, err, result)
+  );
 });
 
 module.exports = router;
